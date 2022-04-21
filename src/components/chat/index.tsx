611448c69074f048858e3bae552e0d17408db543
TheModdedChicken <logan.h.shaw@gmail.com>
2022-04-21 16:37:58 +0800

    Added markdown

--- src/components/chat/index.tsx
+++ src/components/chat/index.tsx
@@ -1,4 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 import React, { useEffect, useRef, useState } from 'react';
 import { ChatMessage } from '../../utility/interfaces';
 import { RoomNameRegex, UsernameRegex } from '../../utility/regex';
@@ -18,7 +19,7 @@ function Chat(props: {username: string, userCount: number, messages: ChatMessage
   for (const message of props.messages) {
     messageElements.push(<li className="chat-message">
       <h4>{message.author}:</h4>
-      <p>{message.message}</p>
+      <p><ReactMarkdown className="mrkdown">{message.message}</ReactMarkdown></p>
     </li>)
   }
 
